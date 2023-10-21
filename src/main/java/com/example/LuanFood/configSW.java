bda9563e66dd545839aec97119b11303bf5f3316
viet <viet1234561231@gmail.com>
2023-10-21 12:55:32 +0700

    d

---
+++ src/main/java/com/example/LuanFood/configSW.java
@@ -0,0 +1,37 @@
+package com.example.LuanFood;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(),
+                description = "\n\n<h1>Đã xong voucher</h1>\n",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                        description = "Local EVM",
+                        url = "http://localhost:8081"
+                ),
+                @Server(
+                        description = "PROD EVM",
+                        url = "https://thangdev.online"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        description = "JWT auth"
+)
+public class configSW {
+}
